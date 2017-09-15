701f139d46220f804533a149801e927d90b99d28
Stephen Warton <stephen.warton@gmail.com>
2017-09-15 13:12:15 -0600

    removed notnullable for article title

--- migrations/20170911133153_article.js
+++ migrations/20170911133153_article.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('article', (table) => {
     table.increments('id').primary();
-    table.string('title').notNullable();
+    table.string('title');
     table.text('content').notNullable();
     table.string('url').notNullable();
     table.integer('users_id').references('users.id').unsigned().onDelete('cascade');
